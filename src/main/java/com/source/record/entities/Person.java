0fdfcae42b3805d59a996d44b43ee60d7341b182
LAPTOP-FAOON65E\Lowie <netzon.lowie@gmail.com>
2018-09-28 15:04:05 +0800

    remove unused import

--- src/main/java/com/source/record/entities/Person.java
+++ src/main/java/com/source/record/entities/Person.java
@@ -9,7 +9,6 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
